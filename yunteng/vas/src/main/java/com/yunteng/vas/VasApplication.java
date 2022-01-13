e1eed94c11a01a53d5be4d8c46554d5b0200891e
HeJiYao <62128205+HeJiYao@users.noreply.github.com>
2022-01-13 18:05:45 +0800

    Add files via upload

---
+++ yunteng/vas/src/main/java/com/yunteng/vas/VasApplication.java
@@ -0,0 +1,14 @@
+package com.yunteng.vas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author Administrator
+ */
+@SpringBootApplication
+public class VasApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VasApplication.class, args);
+    }
+}
---
+++ yunteng/vas/src/test/java/com/yunteng/vas/VasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunteng.vas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VasApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
